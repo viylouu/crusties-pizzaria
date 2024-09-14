217faea75bc221fa6e69b654d756bd37751e1eb1
viylouu <honeefunnee@gmail.com>
2024-09-14 16:01:55 -0600

    Add project files.

---
+++ src/global.cs
@@ -0,0 +1,4 @@
+﻿global using System.Numerics;
+global using SimulationFramework;
+global using SimulationFramework.Drawing;
+global using SimulationFramework.Input;
---
+++ src/main.cs
@@ -0,0 +1,6 @@
+﻿partial class crust {
+    static void Main() {
+        Simulation sim = Simulation.Create(init, rend);
+        sim.Run();
+    }
+}
